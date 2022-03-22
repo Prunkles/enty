cc89a336fd484d75497320a9488f7598e3913f0c
Prunkles <prunkles.dreemurr@gmail.com>
2022-03-22 23:22:25 +0200

    refactor: Move Sense.format to SenseFormatting.fs

--- src/WebApp/src/enty.WebApp/EntityCreating.fs
+++ src/WebApp/src/enty.WebApp/EntityCreating.fs
@@ -1,7 +1,6 @@
 module enty.WebApp.EntityCreating
 
 open System
-open System.Text
 open System.Text.RegularExpressions
 
 open Fable.Core
@@ -17,79 +16,12 @@ open Feliz.MaterialUI.Mui5
 
 open enty.Core
 open enty.Utils
+
 open enty.WebApp
 open enty.WebApp.Utils
-open MindApiImpl
+open enty.WebApp.SenseFormatting
+open enty.WebApp.MindApiImpl
 
-
-[<RequireQualifiedAccess>]
-module Sense =
-
-    let private isValueSimple (value: string) =
-        value
-        |> Seq.forall ^fun c ->
-            Char.IsLetter(c)
-            || Char.IsDigit(c)
-            || c = '-' || c = '_'
-
-    let format (sense: Sense) : string =
-        let sb = StringBuilder()
-        let rec printSense (sb: StringBuilder) sense =
-            match sense with
-            | Sense.Value v ->
-                if isValueSimple v
-                then sb.Append(v) |> ignore
-                else sb.Append('"').Append(v).Append('"') |> ignore
-            | Sense.List l ->
-                sb.Append('[') |> ignore
-                sb.Append(' ') |> ignore
-                for e in l do
-                    printSense sb e
-                    sb.Append(' ') |> ignore
-                sb.Append(']') |> ignore
-            | Sense.Map m ->
-                sb.Append('{') |> ignore
-                sb.Append(' ') |> ignore
-                for KeyValue (k, v) in m do
-                    sb.Append(k).Append(' ') |> ignore
-                    printSense sb v
-                    sb.Append(' ') |> ignore
-                sb.Append('}') |> ignore
-        printSense sb sense
-        sb.ToString()
-
-    let formatMultiline (sense: Sense) : string =
-        let rec appendSense (sb: StringBuilder) (indent: int) (sense: Sense) =
-            let append (s: string) = sb.Append(s) |> ignore
-            let appendLineIndent (s: string) = sb.AppendLine(s).Append(String(' ', 4 * indent)) |> ignore
-            let appendLineIndentIndented (s: string) = sb.AppendLine(s).Append(String(' ', 4 * (indent + 1))) |> ignore
-            let appendSenseIndented sense = appendSense sb (indent + 1) sense
-            match sense with
-            | Sense.Value value ->
-                if isValueSimple value
-                then sb.Append(value) |> ignore
-                else sb.Append('"').Append(value).Append('"') |> ignore
-            | Sense.List list ->
-                append "["
-                for value in list do
-                    appendLineIndentIndented ""
-                    appendSenseIndented value
-                appendLineIndent ""
-                append "]"
-            | Sense.Map map ->
-                append "{"
-                for KeyValue (key, value) in map do
-                    appendLineIndentIndented ""
-                    append key
-                    append " "
-                    appendSenseIndented value
-                appendLineIndent ""
-                append "}"
-        let sb = StringBuilder()
-        appendSense sb 0 sense
-        sb.ToString()
-
-// ----
 
 type ImageShapeUrlStatus =
     | Empty
---
+++ src/WebApp/src/enty.WebApp/SenseFormatting.fs
@@ -0,0 +1,72 @@
+module enty.WebApp.SenseFormatting
+
+open System
+open System.Text
+open enty.Core
+
+[<RequireQualifiedAccess>]
+module Sense =
+
+    let private isValueSimple (value: string) =
+        value
+        |> Seq.forall ^fun c ->
+            Char.IsLetter(c)
+            || Char.IsDigit(c)
+            || c = '-' || c = '_'
+
+    let format (sense: Sense) : string =
+        let sb = StringBuilder()
+        let rec printSense (sb: StringBuilder) sense =
+            match sense with
+            | Sense.Value v ->
+                if isValueSimple v
+                then sb.Append(v) |> ignore
+                else sb.Append('"').Append(v).Append('"') |> ignore
+            | Sense.List l ->
+                sb.Append('[') |> ignore
+                sb.Append(' ') |> ignore
+                for e in l do
+                    printSense sb e
+                    sb.Append(' ') |> ignore
+                sb.Append(']') |> ignore
+            | Sense.Map m ->
+                sb.Append('{') |> ignore
+                sb.Append(' ') |> ignore
+                for KeyValue (k, v) in m do
+                    sb.Append(k).Append(' ') |> ignore
+                    printSense sb v
+                    sb.Append(' ') |> ignore
+                sb.Append('}') |> ignore
+        printSense sb sense
+        sb.ToString()
+
+    let formatMultiline (sense: Sense) : string =
+        let rec appendSense (sb: StringBuilder) (indent: int) (sense: Sense) =
+            let append (s: string) = sb.Append(s) |> ignore
+            let appendLineIndent (s: string) = sb.AppendLine(s).Append(String(' ', 4 * indent)) |> ignore
+            let appendLineIndentIndented (s: string) = sb.AppendLine(s).Append(String(' ', 4 * (indent + 1))) |> ignore
+            let appendSenseIndented sense = appendSense sb (indent + 1) sense
+            match sense with
+            | Sense.Value value ->
+                if isValueSimple value
+                then sb.Append(value) |> ignore
+                else sb.Append('"').Append(value).Append('"') |> ignore
+            | Sense.List list ->
+                append "["
+                for value in list do
+                    appendLineIndentIndented ""
+                    appendSenseIndented value
+                appendLineIndent ""
+                append "]"
+            | Sense.Map map ->
+                append "{"
+                for KeyValue (key, value) in map do
+                    appendLineIndentIndented ""
+                    append key
+                    append " "
+                    appendSenseIndented value
+                appendLineIndent ""
+                append "}"
+        let sb = StringBuilder()
+        appendSense sb 0 sense
+        sb.ToString()
