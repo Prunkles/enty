dee4e010afad0d635036439eea834f2e69b868ba
Prunkles <prunkles.dreemurr@gmail.com>
2022-06-20 03:00:30 +0300

    test: Add some basic tests for Wish parsing

---
+++ src/enty.Utils/String.fs
@@ -0,0 +1,26 @@
+[<RequireQualifiedAccess>]
+module enty.Utils.String
+
+open System
+
+/// <note>
+/// Inspired by C# 11 Raw String Literal feature: https://github.com/dotnet/csharplang/blob/main/proposals/raw-string-literal.md
+/// </note>
+let rawMultiline (str: string) : string =
+    let lines = str.Split([|Environment.NewLine|], StringSplitOptions.None)
+    if not (lines.[0] = "") then invalidArg (nameof str) "The first line must be empty."
+    let lastLine = lines.[lines.Length - 1]
+    if lastLine |> Seq.exists ((<>) ' ') then invalidArg (nameof str) "The last line contains non-whitespace symbols"
+    let indent = lastLine.Length
+    let lines = ArraySegment(lines, 1, lines.Length - 2)
+    let unindentedLines =
+        seq {
+            for idx, line in lines |> Seq.indexed ->
+                if line.Length >= indent && String.IsNullOrWhiteSpace(line.Substring(0, indent)) then
+                    line.Remove(0, indent)
+                elif String.IsNullOrWhiteSpace(line) then
+                    ""
+                else
+                    invalidArg (nameof str) $" The line #{idx} contains symbols out of the last line bound."
+        }
+    String.Join(Environment.NewLine, unindentedLines)
---
+++ tests/enty.Mind.Tests/WishParsing.Tests.fs
@@ -0,0 +1,85 @@
+module enty.Mind.Tests.WishParsing
+
+open Xunit
+open Swensen.Unquote
+
+open enty.Utils
+open enty.Mind
+open enty.Mind.Gateway.WishParsing
+
+
+[<Fact>]
+let ``Explicit And`` () : unit =
+    let wishString = String.rawMultiline """
+        <a & b>
+        """
+    let wish = Wish.parse wishString
+    let expected =
+        Wish.Operator (
+            WishOperator.And (
+                Wish.ValueIs ([], "a"),
+                Wish.ValueIs ([], "b")
+            )
+        )
+    test <@ Ok expected = wish @>
+
+[<Fact(Skip="Not implemented")>]
+let ``Implicit And`` () : unit =
+    let wishString = String.rawMultiline """
+        <a b>
+        """
+    let wish = Wish.parse wishString
+    let expected =
+        Wish.Operator (
+            WishOperator.And (
+                Wish.ValueIs ([], "a"),
+                Wish.ValueIs ([], "b")
+            )
+        )
+    test <@ Ok expected = wish @>
+
+// ----
+
+[<Fact>]
+let ``Parsing <!(a & b)>`` () : unit =
+    let wishString = String.rawMultiline """
+        <!(a & b)>
+        """
+    let wish = Wish.parse wishString
+    let expected =
+        Wish.Operator (
+            WishOperator.And (
+                Wish.Operator (
+                    WishOperator.Not (
+                        Wish.ValueIs ([], "a")
+                    )
+                ),
+                Wish.Operator (
+                    WishOperator.Not (
+                        Wish.ValueIs ([], "b")
+                    )
+                )
+            )
+        )
+    test <@ Ok expected = wish @>
+
+[<Fact>]
+let ``Complex case #1`` () : unit =
+    let wishString = String.rawMultiline """
+        { tags [ a & b ] }
+        """
+    let wish = Wish.parse wishString
+    let expected =
+        Wish.Operator (
+            WishOperator.And (
+                Wish.ListContains (
+                    [ WishPathEntry.MapEntry "tags" ],
+                    "a"
+                ),
+                Wish.ListContains (
+                    [ WishPathEntry.MapEntry "tags" ],
+                    "b"
+                )
+            )
+        )
+    test <@ Ok expected = wish @>
