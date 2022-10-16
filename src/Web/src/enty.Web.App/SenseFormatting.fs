ca7e942b557ecd3cb39cb548e00ce04c26c18a73
Prunkles <prunkles.dreemurr@gmail.com>
2022-10-16 05:18:06 +0300

    refactor: Rewrite sense formatting for top-level braceless sense

--- src/Web/src/enty.Web.App/SenseFormatting.fs
+++ src/Web/src/enty.Web.App/SenseFormatting.fs
@@ -5,6 +5,17 @@ open System.Text
 open enty.Core
 
 [<RequireQualifiedAccess>]
+module Seq =
+
+    let withIsLatest (xs: 'a seq) : (bool * 'a) seq =
+        // Very naive implementation. TODO refactor
+        let n = xs |> Seq.length
+        xs |> Seq.indexed |> Seq.map (fun (i, x) ->
+            let isLatest = i = (n - 1)
+            isLatest, x
+        )
+
+[<RequireQualifiedAccess>]
 module Map =
 
     let tryExactlyOne (map: Map<'k, 'v>) : ('k * 'v) option =
@@ -18,102 +29,139 @@ module Map =
 
 
 [<RequireQualifiedAccess>]
-module SenseValue =
+module SenseValueFormatting =
 
-    let private isAtomSimple (atom: string) =
+    let isAtomSimple (atom: string) =
         if atom = String.Empty then
             false
         else
             atom
             |> Seq.forall ^fun c ->
-                Char.IsLetter(c)
-                || Char.IsDigit(c)
+                Char.IsLetterOrDigit(c)
                 || c = '-' || c = '_'
 
-    let format (senseValue: SenseValue) : string =
-        let sb = StringBuilder()
-        let rec printSense (sb: StringBuilder) sense =
-            match sense with
-            | SenseValue.Atom (SenseAtom a) ->
-                if isAtomSimple a
-                then sb.Append(a) |> ignore
-                else sb.Append('"').Append(a).Append('"') |> ignore
-            | SenseValue.List (SenseList l) ->
-                sb.Append('[') |> ignore
-                sb.Append(' ') |> ignore
-                for e in l do
-                    printSense sb e
-                    sb.Append(' ') |> ignore
-                sb.Append(']') |> ignore
-            | SenseValue.Map (SenseMap m) ->
-                sb.Append('{') |> ignore
-                sb.Append(' ') |> ignore
-                for KeyValue (k, v) in m do
-                    sb.Append(k).Append(' ') |> ignore
-                    printSense sb v
-                    sb.Append(' ') |> ignore
-                sb.Append('}') |> ignore
-        printSense sb senseValue
-        sb.ToString()
+    let append (sb: StringBuilder) (s: string) = sb.Append(s) |> ignore
+    let appendLine (sb: StringBuilder) = sb.AppendLine() |> ignore
 
-    let formatMultiline (senseValue: SenseValue) : string =
-        let rec appendSense (sb: StringBuilder) (indent: int) (senseValue: SenseValue) =
-            let append (s: string) = sb.Append(s) |> ignore
-            let appendLineIndent (s: string) = sb.AppendLine(s).Append(String(' ', 4 * indent)) |> ignore
-            let appendLineIndentIndented (s: string) = sb.AppendLine(s).Append(String(' ', 4 * (indent + 1))) |> ignore
-            let appendSenseIndented sense = appendSense sb (indent + 1) sense
-            let appendSenseValue (value: string) =
-                if isAtomSimple value
-                then sb.Append(value) |> ignore
-                else sb.Append('"').Append(value).Append('"') |> ignore
+    let appendAtom (sb: StringBuilder) (SenseAtom atom) : unit =
+        if isAtomSimple atom then
+            append sb atom
+        else
+            append sb "\""
+            append sb atom
+            append sb "\""
+
+    module Oneline =
+
+        let rec appendMapContent (sb: StringBuilder) (senseMap: SenseMap) : unit =
+            let (SenseMap map) = senseMap
+            for isLatest, (k, v) in map |> Map.toSeq |> Seq.withIsLatest do
+                append sb k
+                append sb " "
+                appendSenseValue sb v
+                if not isLatest then
+                    append sb " "
+
+        and appendSenseValue (sb: StringBuilder) (senseValue: SenseValue) : unit =
             match senseValue with
-            | SenseValue.Atom (SenseAtom value) ->
-                appendSenseValue value
+            | SenseValue.Atom atom ->
+                appendAtom sb atom
+            | SenseValue.List (SenseList l) ->
+                append sb "["
+                append sb " "
+                for e in l do
+                    appendSenseValue sb e
+                    append sb " "
+                append sb "]"
+            | SenseValue.Map senseMap ->
+                append sb "{"
+                append sb " "
+                appendMapContent sb senseMap
+                append sb "}"
+
+    module Multiline =
+
+        let appendIndent (sb: StringBuilder) (indent: int) : unit =
+            append sb (String(' ', 4 * indent))
+
+        let rec appendSenseMapContent (sb: StringBuilder) (indent: int) (senseMap: SenseMap) : unit =
+            let (SenseMap map) = senseMap
+            for isLatest, (key, value) in map |> Map.toSeq |> Seq.withIsLatest do
+                appendIndent sb indent
+                append sb key
+                append sb " "
+                appendSenseValue sb indent value
+                if not isLatest then
+                    appendLine sb
+
+        and appendSenseValue (sb: StringBuilder) (indent: int) (senseValue: SenseValue) : unit =
+            match senseValue with
+            | SenseValue.Atom atom ->
+                appendAtom sb atom
             | SenseValue.List (SenseList list) ->
                 match list with
                 | [] ->
-                    append "[ ]"
-                | [ SenseValue.Atom (SenseAtom a) ] ->
-                    append "[ "
-                    appendSenseValue a
-                    append " ]"
+                    append sb "[ ]"
+                | [ SenseValue.Atom atom ] ->
+                    append sb "[ "
+                    appendAtom sb atom
+                    append sb " ]"
                 | list ->
-                    append "["
-                    for value in list do
-                        appendLineIndentIndented ""
-                        appendSenseIndented value
-                    appendLineIndent ""
-                    append "]"
-            | SenseValue.Map (SenseMap map) ->
+                    append sb "["
+                    appendLine sb
+                    for isLatest, value in list |> List.toSeq |> Seq.withIsLatest do
+                        appendIndent sb (indent + 1)
+                        appendSenseValue sb (indent + 1) value
+                        if not isLatest then
+                            appendLine sb
+                    appendLine sb
+                    appendIndent sb indent
+                    append sb "]"
+            | SenseValue.Map (SenseMap map as senseMap) ->
                 match map with
                 | Map.Empty ->
-                    sb.Append("{ }") |> ignore
-                | Map.ExactlyOne (k, SenseValue.Atom (SenseAtom a)) ->
-                    append "{ "
-                    append k
-                    append " "
-                    appendSenseValue a
-                    append " }"
-                | map ->
-                    append "{"
-                    for KeyValue (key, value) in map do
-                        appendLineIndentIndented ""
-                        append key
-                        append " "
-                        appendSenseIndented value
-                    appendLineIndent ""
-                    append "}"
+                    append sb "{ }"
+                | Map.ExactlyOne (k, SenseValue.Atom atom) ->
+                    append sb "{ "
+                    append sb k
+                    append sb " "
+                    appendAtom sb atom
+                    append sb " }"
+                | _ ->
+                    append sb "{"
+                    appendLine sb
+                    appendSenseMapContent sb (indent + 1) senseMap
+                    appendLine sb
+                    appendIndent sb indent
+                    append sb "}"
+
+
+[<RequireQualifiedAccess>]
+module SenseValue =
+
+    let format (senseValue: SenseValue) : string =
         let sb = StringBuilder()
-        appendSense sb 0 senseValue
-        sb.ToString()
+        SenseValueFormatting.Oneline.appendSenseValue sb senseValue
+        string sb
+
+    let formatMultiline (senseValue: SenseValue) : string =
+        let sb = StringBuilder()
+        SenseValueFormatting.Multiline.appendSenseValue sb 0 senseValue
+        string sb
 
 
 [<RequireQualifiedAccess>]
 module Sense =
 
     let format (sense: Sense) : string =
-        SenseValue.format (Sense.asValue sense)
+        let (Sense senseMap) = sense
+        let sb = StringBuilder()
+        SenseValueFormatting.Oneline.appendMapContent sb senseMap
+        string sb
 
     let formatMultiline (sense: Sense) : string =
-        SenseValue.formatMultiline (Sense.asValue sense)
+        let (Sense senseMap) = sense
+        let sb = StringBuilder()
+        SenseValueFormatting.Multiline.appendSenseMapContent sb 0 senseMap
+        string sb
 
